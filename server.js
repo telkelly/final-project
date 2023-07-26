5703968c6236f679ec258c3297f63a6462ad54e8
Kelly <84016657+telkelly@users.noreply.github.com>
2023-07-26 13:53:31 +0300

    chandeg json files

--- server.js
+++ server.js
@@ -13,10 +13,20 @@ app.use(cors());
 app.use(express.urlencoded({ extended: true })); 
 app.use(express.json());
 app.use(cookieParser());
-app.use('/', express.static('frontend'))
+// app.use('/', express.static('frontend'))
 
 app.listen(process.env.PORT, () => {
     console.log(`run on ${process.env.PORT}`);
 })
 
 app.use(user_routes.router)
+
+// Have Node serve the files for our built React app
+// app.use(express.static(path.resolve(__dirname, "./client/build")));
+app.use(express.static(path.join(__dirname, "frontend/build")));
+
+// All other GET requests not handled before will return our React app
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "./frontend/build", "index.html"));
+});
+
